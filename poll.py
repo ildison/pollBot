ce0a2cc2a0f8ff125128074dc7eba5c6ddd98d33
ildison <ildarusmanov92@gmail.com>
2020-12-10 18:49:21 +0300

    add prints and change time

--- poll.py
+++ poll.py
@@ -8,6 +8,7 @@ chat_id = environ["TESTGROUPID"]
 updater = Updater(token=token)
 
 def poller(context):
+    print("PollBot will create a new poll")
     bot = context.bot
     today = datetime.now().strftime("%d.%m")
     question = f"Футбол. {today}"
@@ -15,8 +16,11 @@ def poller(context):
         bot.send_poll(chat_id=chat_id, question=question, options=["+", "-"], is_anonymous=False)
     except Exception as error:
         print(error)
+    print(f"PollBot created a new poll with question {question}")
 
-time = time(18,44,40, 0000)
+time = time(15,55,55, 0000)
 updater.dispatcher.job_queue.run_daily(poller, time, days=(0,1,2,3,4,5,6))
 updater.dispatcher.job_queue.start()
+print("PollBot started")
 updater.idle()
+print("PollBot finished")
