e6cb6f86fa2caddd391593169d2016bc42154afa
ildison <ildarusmanov92@gmail.com>
2020-12-10 21:18:15 +0300

    add pin/unpin message

--- poll.py
+++ poll.py
@@ -6,21 +6,43 @@ token = environ["TOKEN"]
 chat_id = environ["TESTGROUPID"]
 
 updater = Updater(token=token)
+jobQueue = updater.dispatcher.job_queue
+pollBot = updater.bot
+
+def unpinMessage(context):
+    message_id = context.job.context.message_id
+    chat_id = context.job.context.chat_id
+
+    pollBot.unpin_chat_message(chat_id, message_id)
+    print("PollBot unpined message")
+
+def pinMessage(message):
+    message_id = message.message_id
+    chat_id = message.chat_id
+
+    pollBot.pin_chat_message(chat_id, message_id)
+    print("PollBot pined message")
+
+def scheduleUnpinningMessage(message):
+    jobQueue.run_once(unpinMessage, context=message, when=200)
+    print("PollBot scheduled unpinning message")
 
 def poller(context):
-    print("PollBot will create a new poll")
-    bot = context.bot
     today = datetime.now().strftime("%d.%m")
     question = f"Футбол. {today}"
+
+    print("PollBot will create a new poll")
     try:
-        bot.send_poll(chat_id=chat_id, question=question, options=["+", "-"], is_anonymous=False)
+        message = pollBot.sendPoll(chat_id=chat_id, question=question, options=["+", "-"], is_anonymous=False)
     except Exception as error:
         print(error)
     print(f"PollBot created a new poll with question {question}")
+    pinMessage(message)
+    scheduleUnpinningMessage(message)
 
-time = time(16,29,55, 0000)
-updater.dispatcher.job_queue.run_daily(poller, time, days=(0,1,2,3,4,5,6))
-updater.dispatcher.job_queue.start()
+time = time(18,00,50, 0000)
+jobQueue.run_daily(poller, time, days=(0,1,2,3,4,5,6))
+jobQueue.start()
 print("PollBot started")
 updater.idle()
 print("PollBot finished")
